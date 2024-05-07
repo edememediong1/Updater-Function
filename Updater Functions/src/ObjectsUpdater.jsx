79d738bdeaaa8f20fffc310b0d52db1718ba1805
Emediong Bassey Edem <edememediong1@gmail.com>
2024-05-07 23:59:09 +0100

    space removed

--- Updater Functions/src/ObjectsUpdater.jsx
+++ Updater Functions/src/ObjectsUpdater.jsx
@@ -15,7 +15,6 @@ export default function ObjectsUpdate(){
     return(
         <div>
                 <p>Your favourite car is: {car.year} {car.make} {car.model}</p>
-
                 <input type="number" value={car.year}  onChange={}/><br/>
                 <input type="text" value={car.name} onChange={}/> <br/>
                 <input type="text" value={car.model} onChange={}/> <br/>
