ecad3bfba3960d7428c47ed478f22bf96bfad574
Emediong Bassey Edem <edememediong1@gmail.com>
2024-05-08 15:57:54 +0100

    Make update to the ArraysUpdate Component

--- Updater Functions/src/ArraysUpdate.jsx
+++ Updater Functions/src/ArraysUpdate.jsx
@@ -4,24 +4,24 @@ export default function ArraysUpdate(){
     const [foods, setFoods] = useState(["Apple", "Orange", "Banana"])
 
 
-    const handleAddFood = ()=>{
+    function handleAddFood() {
         const newFood = document.getElementById('foodInput').value;
-        document.getElementById('foodInput').value = ''
+        document.getElementById('foodInput').value = ""
 
-        setFoods(f => [...f, newFood])
+        setFoods(foods => ([...foods, newFood]))
     }
 
-    const handleRemoveFood = () => {
-        
+    const handleRemoveFood = (index) => {
+        setFoods(foods.filter((_, i) => i !== index))
     }
     return(
         <div>
             <h2>List of Food</h2>
 
             <ul>
-                {foods.map((food,index) => <li key={index}>{food}</li>)}   
+                {foods.map((food,index) => <li key={index} onClick={() => handleRemoveFood(index)}>{food}</li>)}   
             </ul>
-            <input type="text" id="footInput" placeholder="Enter Food..." ></input>
+            <input type="text" id="foodInput" placeholder="Enter Food..." />
             <button onClick={handleAddFood}>Add Food</button>
         </div>
     )
