08ba9f04f1b07501b107912291b3719af241080a
Emediong Bassey Edem <edememediong1@gmail.com>
2024-05-09 23:53:09 +0100

    update #2

--- Updater Functions/src/component/ArraysObject.jsx
+++ Updater Functions/src/component/ArraysObject.jsx
@@ -8,23 +8,27 @@ export default function ArraysObject(){
 
 
     function handleAddCar() {
-
+       const newCar = {year: carYear,
+                        make: carMake,
+                        model: carModel
+                        } 
+            setCars(c => [...c, newCar])
     }
 
     function handleRemoveCar(){
  
     }
 
-    function handleYearChange(){
-
+    function handleYearChange(event){
+        setCarYear(event.target.value)
     }
 
-    function handleMakeChange(){
-
+    function handleMakeChange(event){
+        setCarMake(event.target.value)
     }
 
-    function handleModelChange(){
-
+    function handleModelChange(event){
+        setCarModel(event.target.value)
     }
 
     
@@ -32,12 +36,16 @@ export default function ArraysObject(){
         <section>
             <h2> List of Car Objects</h2>
             <ul>
-
+                {cars.map((car, index) => 
+                    <li key={index}> 
+                        {car.year}{car.make}{car.model}
+                    </li>
+                )}
             </ul> <br/>
             <input type="number" value={carYear} onChange={handleYearChange} /><br/>
             <input type="text" value={carMake} onChange={handleMakeChange} placeholder="Enter car make..."/> <br/>
             <input type="text" value={carModel} onChange={handleModelChange} placeholder="Enter car model..."/> <br/>
-            
+            <button onClick={handleAddCar}>Submit</button>
         </section>
     )
 }
